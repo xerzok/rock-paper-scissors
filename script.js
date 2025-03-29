c7cd90c05c11670674dff3c4c21b4a1177da9cf4
xerzok <84401936+xerzok@users.noreply.github.com>
2025-03-29 10:27:03 +0400

    coded getHumanChoice()

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ console.log("Hi")
 function getComputerChoice () {
     let x = Math.random()*100;
     if (x<33){
-        return "scissors"
+        return "rock"
     }
     else if (33<=x && x<66) {
         return "paper"
     }
     else {
-        return "rock"
+        return "scissors"
     }
+}
+function getHumanChoice (){let message = prompt ("Type 1 for rock, 2 for paper or 3 for scissors")
+    if (message == 1){
+        return "rock";
+    }
+    else if (message == 2){
+        return "paper";
+    }
+    else {return "scissors";}
 }
